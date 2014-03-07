d426ee62ee3d60ae1d9ae930f0d846e96bf814dc
Ryan DV <rdevilla@uwaterloo.ca>
2014-03-07 09:50:41 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,2 @@
+# nulogyFacePage
+### a Sails application
---
+++ api/models/Employee.js
@@ -0,0 +1,25 @@
+/**
+ * Employee.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs                :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    lastName: "string",
+    firstName: "string",
+    department: "string",
+    email: "string",
+    nulogyBirthday: "datetime",
+
+    workedOn: {
+      collection: "project",
+      via: "contributors",
+      dominant: true
+    }
+
+  }
+
+};
